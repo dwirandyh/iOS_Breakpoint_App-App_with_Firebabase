82dec84d75aaef4022c817b29a92baf7aaaff638
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-03-12 14:08:24 +0700

    Initialize Project

---
+++ breakpoint/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  breakpoint
+//
+//  Created by Dwi Randy Herdinanto on 12/03/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ breakpoint/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  breakpoint
+//
+//  Created by Dwi Randy Herdinanto on 12/03/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
